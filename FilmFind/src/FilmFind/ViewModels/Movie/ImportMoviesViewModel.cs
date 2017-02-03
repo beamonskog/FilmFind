589abbf31078ae2a2a83e8a72568712e3c40d469
skogg <skogskogskog@gmail.com>
2017-02-03 14:44:18 +0100

    not sure what this is

---
+++ FilmFind/src/FilmFind/ViewModels/Movie/ImportMoviesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace FilmFind.ViewModels.Movie
+{
+    public class ImportMoviesViewModel
+    {
+        public List<ImportMovieViewModel> ImportMovies { get; set; }
+        public string Message { get; set; }
+
+        public ImportMoviesViewModel()
+        {
+            ImportMovies = new List<ImportMovieViewModel>();
+        }
+    }
+}
